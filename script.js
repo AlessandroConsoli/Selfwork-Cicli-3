d2569bb0bbe46e41c24319289bbf9b5077be12e8
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-08-21 18:22:03 +0200

    versione finale

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// let selected = Number(prompt(`Inserisci il numero corrispondente alla bevanda`));
-
-// switch (selected) {
-//     case 1:
-//         alert(`E’ stata selezionata Acqua`);
-//         break;
-//     case 2:
-//         alert(`E’ stata selezionata Coca cola`);
-//         break;
-//     case 3:
-//         let age = prompt(`Hai selezionato un alcolico. Quanti anni hai?`);
-//             if (age >= 18) {
-//                 alert(`Birra in erogazione`);
-//             }else{
-//                 alert(`Sei minorenne, non puoi bere alcolici`);
-//             }
-//         break;
-//     case typeof selected != Number:
-//         alert(`Devi inserire il numero corrispondente alla bevanda`)
-//         break;
-//     default:
-//         alert(`Scelta non valida`);
-//         break;
-// }
-    //    EXTRA
-
 let selected;
 
 do {
